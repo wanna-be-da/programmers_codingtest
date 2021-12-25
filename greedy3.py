f116860560f615a0e03cc27971c977ef95e766a9
wanna-be-da <ceh9856@snu.ac.kr>
2021-12-26 00:00:34 +0900

    20211225

--- greedy3.py
+++ greedy3.py
@@ -1,14 +1,14 @@
 def solution(number, k):
+    del_index = 1
     while k > 0:
-        del_number = number[0]
-        del_index = 0
-        for i in range(1, len(number)):
-            if number[i] > del_number:
+        for i in range(max(0, del_index - 1), len(number) - 1):
+            if number[i] < number[i + 1]:
+                number = number[:i] + number[i + 1:]
+                del_index = i
+                k -= 1
                 break
-            del_number = number[i]
-            del_index = i
+            if i == len(number) - 2:
+                return number[:-k]
 
-        number = number[:del_index] + number[del_index + 1:]
-        k -= 1
 
     return number
